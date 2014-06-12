e4765a03427943857e221878c7e50e1c359ac4ea
Inge Jørgensen <inge@manualdesign.no>
2014-06-12 03:17:58 +0200

    Encapsulated model data in a separate object

--- lib/shrouded/model.rb
+++ lib/shrouded/model.rb
@@ -1,24 +1,28 @@
-require 'shrouded/model/callbacks'
 require 'shrouded/model/class_methods'
+require 'shrouded/model/data'
 
 module Shrouded
   module Model
     extend ActiveSupport::Concern
-    include Shrouded::Model::Callbacks
+
+    included do
+      before_save :store_data
+      after_save :cleanup_data
+      after_destroy :delete_data
+    end
 
     def data
-      @_cached_data ||= data_from(closest_data)
+      shrouded_data.cached
     end
 
     def data?
-      raw_data? || stored_data?
+      shrouded_data.any?
     end
 
     def data=(new_data)
-      self.raw_data = new_data
-      @_cached_data = nil
+      shrouded_data.set(new_data)
       shrouded_set :content_hash, nil
-      shrouded_set :content_length, detect_content_length
+      shrouded_set :content_length, shrouded_data.content_length
     end
 
     def file=(file)
@@ -29,68 +33,36 @@ module Shrouded
 
     private
 
-    def shrouded_set(attribute_name, value)
-      self[shrouded_attribute(attribute_name)] = value
+    def cleanup_data
+      if previous_hash = changes[shrouded_attribute(:content_hash)].try(&:first)
+        shrouded_data.expire(previous_hash)
+      end
+    end
+
+    def delete_data
+      shrouded_data.expire(shrouded_get(:content_hash))
+    end
+
+    def store_data
+      if shrouded_data.changed?
+        shrouded_set :content_hash, shrouded_data.store!
+      end
     end
 
     def shrouded_get(attribute_name)
       self[shrouded_attribute(attribute_name)]
     end
 
+    def shrouded_data
+      @shrouded_data ||= Shrouded::Model::Data.new(self)
+    end
+
+    def shrouded_set(attribute_name, value)
+      self[shrouded_attribute(attribute_name)] = value
+    end
+
     def shrouded_attribute(attribute_name)
       self.class.shrouded_attributes[attribute_name]
-    end
-
-    def shrouded_type
-      self.class.shrouded_type
-    end
-
-    def data_from(object)
-      return nil unless object
-      if object.respond_to?(:body)
-        object.body
-      elsif object.respond_to?(:read)
-        object.rewind
-        object.read
-      else
-        object
-      end
-    end
-
-    def closest_data
-      if raw_data?
-        raw_data
-      elsif stored_data?
-        stored_data
-      end
-    end
-
-    def detect_content_length
-      if raw_data.respond_to?(:length)
-        raw_data.length
-      else
-        data.try(&:length).to_i
-      end
-    end
-
-    def raw_data?
-      raw_data ? true : false
-    end
-
-    def raw_data
-      @raw_data
-    end
-
-    def raw_data=(new_data)
-      @raw_data = new_data
-    end
-
-    def stored_data?
-      !shrouded_get(:content_hash).blank?
-    end
-
-    def stored_data
-      Shrouded::Storage.get(shrouded_type, shrouded_get(:content_hash))
     end
   end
 end
---
+++ lib/shrouded/model/data.rb
@@ -0,0 +1,97 @@
+module Shrouded
+  module Model
+    class Data
+      attr_reader :record, :raw
+
+      def initialize(record)
+        @record = record
+        @raw = nil
+        @cached = nil
+      end
+
+      def any?
+        raw? || stored?
+      end
+
+      def cached
+        @cached ||= read_from(closest)
+      end
+
+      def changed?
+        raw?
+      end
+
+      def content_length
+        if raw? && raw.respond_to?(:length)
+          raw.length
+        else
+          cached.try(&:length).to_i
+        end
+      end
+
+      def expire(hash)
+        unless record.class.where(
+          record.class.shrouded_attributes[:content_hash] => hash
+        ).any?
+          Shrouded::Storage.delete(
+            record.class.shrouded_type,
+            hash
+          )
+        end
+      end
+
+      def raw?
+        raw ? true : false
+      end
+
+      def set(new_data)
+        @cached = nil
+        @raw = new_data
+      end
+
+      def store!
+        Shrouded::Storage.store(
+          record.class.shrouded_type,
+          raw
+        )
+      end
+
+      private
+
+      def closest
+        if raw?
+          raw
+        elsif stored?
+          stored
+        end
+      end
+
+      def content_hash
+        record[record.class.shrouded_attributes[:content_hash]]
+      end
+
+      def read_from(object)
+        return nil unless object
+        if object.respond_to?(:body)
+          object.body
+        elsif object.respond_to?(:read)
+          object.rewind
+          object.read
+        else
+          object
+        end
+      end
+
+      def stored?
+        !content_hash.blank?
+      end
+
+      def stored
+        Shrouded::Storage.get(
+          record.class.shrouded_type,
+          content_hash
+        )
+      end
+    end
+  end
+end
