025a9778117b189b2fabc6acb3c86048b95ae966
Inge Jørgensen <inge@elektronaut.no>
2023-09-01 23:05:56 +0200

    Use the content hash for data presence validation
    This avoids fetching the data from storage.

--- lib/dis/validations/data_presence.rb
+++ lib/dis/validations/data_presence.rb
@@ -8,7 +8,15 @@ module Dis
       # Validates that a record has data, either freshly assigned or
       # persisted in the storage. Adds a `:blank` error on `:data`if not.
       def validate(record)
-        record.errors.add(:data, :blank) if !record.data? || record.data.blank?
+        return if record.data? && record.content_hash != self.class.empty_hash
+
+        record.errors.add(:data, :blank)
+      end
+
+      class << self
+        def empty_hash
+          @empty_hash ||= Dis::Storage.file_digest("")
+        end
       end
     end
   end
